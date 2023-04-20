3e8f7bf872da4dba1ce76d035647d3360d9b1eed
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-20 18:58:48 +0300

    this alivE!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import axios from "axios";
+
+function getRandomHexColor() {
+  return `#${Math.floor((0.2+0.5*Math.random()) * 16777215).toString(16)}`;
+}
+
+const appStyles = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '24px',
+  gap: '12px',
+  color: '#010101',
 };
+axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
+
+const ArticleList = ({ articles }) => (
+  <ul>
+    {articles.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+export class App extends Component {
+  state = {
+    articles: [],
+  };
+
+  async componentDidMount() {
+    const response = await axios.get("/search?query=react");
+    this.setState({ articles: response.data.hits });
+  }
+
+  render() {
+    const { articles } = this.state;
+    return (
+      <div style={{ ...appStyles, backgroundColor: getRandomHexColor() }}>
+        articles.length > 0 ? <ArticleList articles={articles} /> : null
+      </div>
+    );
+  }
+}
+
+
